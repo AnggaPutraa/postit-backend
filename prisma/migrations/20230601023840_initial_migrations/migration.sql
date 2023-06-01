afa8c0a7c16c04019628bf8036f3a9aad84f626c
AnggaPutraa <yananggaa@gmail.com>
2023-06-01 09:44:25 +0700

    refactor: local signup

--- prisma/migrations/20230601023840_initial_migrations/migration.sql
+++ prisma/migrations/20230601023840_initial_migrations/migration.sql
@@ -2,10 +2,10 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
     "hashedPassword" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "hashedRefreshToken" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { Tokens } from './types/_index';
+import * as bycrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Injectable()
 export class AuthService {
@@ -8,12 +12,73 @@ export class AuthService {
         private prisma: PrismaService,
         private jwtService: JwtService) { }
 
-    async localSignUp() { }
+    async localSignUp(data: AuthDto): Promise<Tokens> {
+        const hashedPassword = await this.hashData(data.password);
+        const newUser = await this.prisma.user.create({
+            data: {
+                email: data.email,
+                hashedPassword
+            }
+        });
+
+        const tokens = await this.getTokens(newUser.id, newUser.email);
+        await this.updateHashedRefreshToken(newUser.id, tokens.refresh_token);
+        return tokens;
+    }
 
     async localSignIn() { }
 
     async logout() { }
 
     async refresh() { }
+
+    private hashData(data: string) {
+        return bycrypt.hash(data, 10);
+    }
+
+    private async getTokens(userId: string, email: string) {
+        const [access_token, refresh_token] = await Promise.all([
+            this.jwtService.signAsync(
+                {
+                    sub: userId,
+                    email,
+                },
+                {
+                    expiresIn: 60 * 15,
+                    secret: process.env.ACCESS_TOKEN_SECRET,
+                },
+            ),
+            this.jwtService.signAsync(
+                {
+                    sub: userId,
+                    email,
+                },
+                {
+                    expiresIn: 60 * 60 * 24 * 7,
+                    secret: process.env.REFRESH_TOKEN_SECRET,
+                },
+            ),
+        ]);
+
+        return {
+            access_token,
+            refresh_token,
+        };
+    }
+
+    private async updateHashedRefreshToken(
+        userId: string,
+        refreshToken: string
+    ) {
+        const hashedRefreshToken = await this.hashData(refreshToken);
+        await this.prisma.user.update({
+            where: {
+                id: userId,
+              },
+              data: {
+                hashedRefreshToken
+              },
+        });
+    }
 
 }
