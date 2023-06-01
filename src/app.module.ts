fd5a5cc2c691b8caa42174bc5c2fbaf4f4770ddc
AnggaPutraa <yananggaa@gmail.com>
2023-06-01 12:59:28 +0700

    feat: post modoule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AccessTokenGuard } from './common/guards/access_token.guard';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, PostModule],
   controllers: [],
   providers: [
     {
