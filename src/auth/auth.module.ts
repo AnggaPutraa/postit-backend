67f917c2f9875615b98427e4902d6cca625eab97
AnggaPutraa <yananggaa@gmail.com>
2023-06-01 09:16:25 +0700

    refactor: typings

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService]
+  imports: [JwtModule.register({})],
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
